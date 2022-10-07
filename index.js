b9e8a8622068c031a11fcbfde1887bd129fcbcc9
thoanguyen2907 <thoanguyen290795@gmail.com>
2022-10-07 13:42:25 +0300

    database connected

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
-const express = require("express");
-const connectDatabase = require("./config/db")
-const productRouter = require("./routes/products.route");
-const PORT = 8000
-const app = express();
+const Express = require ("express");
+const mongoose = require ("mongoose");
+const productRouter = require ("./routes/products.route");
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+const PORT = 8005
 
-//ROUTE
+const uri = "mongodb+srv://thoa-nguyen:thoanguyen@nodejstraining.4cyhs.mongodb.net/demo-isa?retryWrites=true&w=majority";
+  mongoose.connect(
+    uri,
+    (e) => {
+      try {
+        if (e) {
+          throw e;
+        }
+        console.log("database connected");
+        app.listen(
+            PORT, () => {
+            console.log(
+              "running on port "
+            )
+          }
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  );
+ 
+const app = Express();
+app.use(Express.json())
+app.use(Express.urlencoded({extended: true}))
+
+// routes here
 app.use("/products", productRouter);
+// app.use("/products", productRouter);
 
-app.listen(PORT, async () => {
-    console.log(`Server is running http://localhost:${PORT}`)
-    await connectDatabase()
-})
+
+
+
+
+
+
+
+
+
+
+
